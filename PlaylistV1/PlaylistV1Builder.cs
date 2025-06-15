531362fc61d2f111c91d8f4b24747ca2744cd350
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2025-06-15 07:13:19 -0700

    Update docs

--- PlaylistV1/PlaylistV1Builder.cs
+++ PlaylistV1/PlaylistV1Builder.cs
@@ -14,17 +14,7 @@ namespace VS.TestPlaylistTools.PlaylistV1
         /// </summary>
         /// <param name="testNames">The test names to include in the playlist.</param>
         /// <returns>A new PlaylistV1 object.</returns>
-        public static PlaylistRoot Create(IEnumerable<string>? testNames = null)
-        {
-            return new PlaylistRoot(testNames ?? []);
-        }
-
-        /// <summary>
-        /// Creates a new playlist with the specified test names.
-        /// </summary>
-        /// <param name="testNames">The test names to include in the playlist.</param>
-        /// <returns>A new PlaylistV1 object.</returns>
-        public static PlaylistRoot Create(params string[] testNames)
+        public static PlaylistRoot Create(List<string>? testNames)
         {
             return new PlaylistRoot(testNames ?? []);
         }
