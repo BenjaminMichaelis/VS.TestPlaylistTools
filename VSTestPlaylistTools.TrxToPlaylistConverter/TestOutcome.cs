b2b432d8f9eaa0a8e7e489e9792075af351094fd
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2026-05-16 14:00:04 -0700

    Enhance TestOutcome enum with additional statuses

--- VSTestPlaylistTools.TrxToPlaylistConverter/TestOutcome.cs
+++ VSTestPlaylistTools.TrxToPlaylistConverter/TestOutcome.cs
@@ -2,20 +2,82 @@ namespace VSTestPlaylistTools.TrxToPlaylist;
 
 /// <summary>
 /// Represents the outcome of a test result from a TRX file.
+/// https://github.com/microsoft/vstest/blob/ba0077af37594366d76e0b8455143b982de3d416/src/Microsoft.TestPlatform.Extensions.TrxLogger/ObjectModel/TestOutcome.cs#L14-L90
 /// </summary>
 public enum TestOutcome
 {
-    Passed,
-    Failed,
-    NotExecuted,
-    Inconclusive,
-    Timeout,
-    Pending,
+    /// <summary>
+    /// There was a system error while we were trying to execute a test.
+    /// </summary>
     Error,
+
+    /// <summary>
+    /// Test was executed, but there were issues.
+    /// Issues may involve exceptions or failed assertions.
+    /// </summary>
+    Failed,
+
+    /// <summary>
+    /// The test timed out
+    /// </summary>
+    Timeout,
+
+    /// <summary>
+    /// Test was aborted.
+    /// This was not caused by a user gesture, but rather by a framework decision.
+    /// </summary>
     Aborted,
+
+    /// <summary>
+    /// Test has completed, but we can't say if it passed or failed.
+    /// May be used for aborted tests...
+    /// </summary>
+    Inconclusive,
+
+    /// <summary>
+    /// Test was executed w/o any issues, but run was aborted.
+    /// </summary>
+    PassedButRunAborted,
+
+    /// <summary>
+    /// Test had it chance for been executed but was not, as ITestElement.IsRunnable == false.
+    /// </summary>
     NotRunnable,
+
+    /// <summary>
+    /// Test was not executed.
+    /// This was caused by a user gesture - e.g. user hit stop button.
+    /// </summary>
+    NotExecuted,
+
+    /// <summary>
+    /// Test run was disconnected before it finished running.
+    /// </summary>
+    Disconnected,
+
+    /// <summary>
+    /// To be used by Run level results.
+    /// This is not a failure.
+    /// </summary>
     Warning,
+
+    /// <summary>
+    /// Test was executed w/o any issues.
+    /// </summary>
+    Passed,
+
+    /// <summary>
+    /// Test has completed, but there is no qualitative measure of completeness.
+    /// </summary>
     Completed,
+
+    /// <summary>
+    /// Test is currently executing.
+    /// </summary>
     InProgress,
-    Disconnected
+
+    /// <summary>
+    /// Test is in the execution queue, was not started yet.
+    /// </summary>
+    Pending
 }
